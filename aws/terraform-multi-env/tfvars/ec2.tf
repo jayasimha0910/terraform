4ea411da9c3e765a734427275a2cdcfa24187f48
Marapareddy Jayasimha Reddy <marapareddy.jayasimha.reddy@ibm.com>
2025-07-01 19:20:44 +0530

    s-33

---
+++ aws/terraform-multi-env/tfvars/ec2.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "roboshop" {
+  count                  = length(var.instances)
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  //subnet_id = "" - Optional
+  //key_name = "" - Optional
+
+  //tags = var.ec2_tags
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name        = "${var.project}-${var.instances[count.index]}-${var.environment}"
+      Component   = var.instances[count.index]
+      Environment = var.environment
+    }
+  )
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "${var.project}-${var.sg_name}-${var.environment}"
+  description = var.sg_description
+
+  ingress {
+    description      = "Allow all traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "Allow all traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.sg_name}-${var.environment}"
+    }
+  )
+}
+
+
---
+++ aws/terraform-multi-env/tfvars/variables.tf
@@ -0,0 +1,43 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+
+}
+
+variable "common_tags" {
+  default = {
+    Project   = "roboshop"
+    Terraform = "true"
+  }
+}
+
+variable "sg_name" {
+  default = "allow-all"
+
+}
+
+variable "sg_description" {
+  default = "Allowing from all ports"
+
+}
+
+variable "instances" {
+  default = ["mongodb", "redis"]
+
+}
+
+variable "ami_id" {
+  default = "ami-09c813fb71547fc4f"
+
+}
+
+variable "instance_type" {
+  
+  
+}
+
+
+
+
