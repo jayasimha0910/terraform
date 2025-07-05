49208afe66f0aa82fa84ef858cebc943036b1033
Marapareddy Jayasimha Reddy <marapareddy.jayasimha.reddy@ibm.com>
2025-07-05 23:33:33 +0530

    s-37

--- aws/terraform-aws-vpc/data.tf
+++ aws/terraform-aws-vpc/data.tf
@@ -1,3 +1,19 @@
+#Info about AZ's
 data "aws_availability_zones" "available" {
   state = "available"
+}
+
+#Info about Default VPC
+data "aws_vpc" "default" {
+  default = true
+  
+}
+
+#Info about Default VPC Routetable
+data "aws_route_table" "main" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "association.main"
+    values = ["true"]
+  }
 }
--- aws/terraform-aws-vpc/vpc.tf
+++ aws/terraform-aws-vpc/vpc.tf
@@ -1,10 +1,11 @@
-#VPC
+#Create VPC
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
   tags = merge(
+    var.vpc_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}"
@@ -13,11 +14,12 @@ resource "aws_vpc" "main" {
 }
 
 
-#IGW
+#create IGW
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id #accosiation with vpc
 
   tags = merge(
+    var.igw_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}"
@@ -26,7 +28,7 @@ resource "aws_internet_gateway" "main" {
 }
 
 
-#subnet-public
+#create public-subnet
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -38,6 +40,7 @@ resource "aws_subnet" "public" {
 
 
   tags = merge(
+    var.public_subnet_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
@@ -46,7 +49,7 @@ resource "aws_subnet" "public" {
 }
 
 
-#subnet-private
+#create private-subnet
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -57,6 +60,7 @@ resource "aws_subnet" "private" {
 
 
   tags = merge(
+    var.private_subnet_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
@@ -65,7 +69,7 @@ resource "aws_subnet" "private" {
 }
 
 
-#subnet-database
+#Create database-subnet
 resource "aws_subnet" "database" {
   count = length(var.database_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
@@ -75,9 +79,128 @@ resource "aws_subnet" "database" {
   
 
   tags = merge(
+    var.database_subnet_tags,
     local.common_tags,
     {
         Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
     }
   )
 }
+
+#Create EIP 
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+
+  tags = merge(
+    var.eip_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+
+
+#create NAT gateway on public subnet-1a and link to internet gateway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.nat_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}"
+    }
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.main]
+}
+
+
+#create public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.public_route_table_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-public"
+    }
+  )
+}
+
+
+#create private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.private_route_table_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-private"
+    }
+  )
+}
+
+
+#create database route table
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.database_route_table_tags,
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-database"
+    }
+  )
+}
+
+
+#Add internet route to public subnet through internet gateway
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+#Add internet route to private subnet through NAT gateway
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+#Add internet route to database subnet through NAT gateway
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.main.id
+}
+
+#Public Route table association 
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+#Private Route table association 
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+#Database Route table association 
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
