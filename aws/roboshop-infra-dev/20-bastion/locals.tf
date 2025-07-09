0256a9d293e3e528ff16921d5468d17ec8734ec4
Marapareddy Jayasimha Reddy <marapareddy.jayasimha.reddy@ibm.com>
2025-07-09 22:33:41 +0530

    s-39

--- aws/roboshop-infra-dev/20-bastion/locals.tf
+++ aws/roboshop-infra-dev/20-bastion/locals.tf
@@ -1,7 +1,7 @@
 locals {
   ami_id = data.aws_ami.joindevops.id
   bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id
-  public_sbunet_id = split ("," , data.aws_ssm_parameter.public_sbunet_ids.value[0])
+  public_subnet_id = split ("," , data.aws_ssm_parameter.public_subnet_ids.value[0])
 
   common_tags = {
     Name = var.project
